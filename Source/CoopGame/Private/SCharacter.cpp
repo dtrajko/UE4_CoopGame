538f335724bdaf8e18f52785acbebca5800ce767
Dejan Trajkovic <dtrajko@gmail.com>
2019-10-08 12:01:49 +0200

    Section 6: Coop Game - 52. Setup Player Jump

--- Source/CoopGame/Private/SCharacter.cpp
+++ Source/CoopGame/Private/SCharacter.cpp
@@ -77,4 +77,6 @@ void ASCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponen
 
 	PlayerInputComponent->BindAction("Crouch", IE_Pressed, this, &ASCharacter::BeginCrouch);
 	PlayerInputComponent->BindAction("Crouch", IE_Released, this, &ASCharacter::EndCrouch);
+
+	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ASCharacter::Jump);
 }
